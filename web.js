4e6de598ba17cc7851fd597ffd65231d9c439a6e
Felix <felix@ubuntu.(none)>
2013-07-09 14:44:00 +0200

    buffer fixed

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var fs = require('fs')
 
-var buf = buf.toString();
-
-
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html"))
-});
+app.get('/', function(request, response)
+ {
+  response.send(fs.readFileSync("./index.html").toString());
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
