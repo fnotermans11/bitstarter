01adf863f317cb8300b9d8379ddad030d2df48ff
Felix <felix@ubuntu.(none)>
2013-07-09 14:31:36 +0200

    buf.tostring

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var fs = require('fs')
-buf = new Buffer(256);
-len = buf.write('\u00bd + \u00bc = \u00be', 0);
-console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+var buf = buf.toString();
 
 
 var app = express.createServer(express.logger());
