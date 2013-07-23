6bf7f628e09e4a06061ea232f09aecdfed04c06a
fnotermans11 <fnotermans11@gmail.com>
2013-07-23 20:58:12 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response)
  {
   response.send(fs.readFileSync("./index.html").toString());
  });
+
+var oneDay = 864000000;
+
+app.use(express.compress());
+
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
