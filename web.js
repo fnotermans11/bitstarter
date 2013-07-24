33e9335f653c716e851bfc29b5095f3d788952b7
fnotermans11 <fnotermans11@gmail.com>
2013-07-24 13:12:48 +0000

    Fixed images

--- web.js
+++ web.js
@@ -8,6 +8,9 @@ app.get('/', function(request, response)
   response.send(fs.readFileSync("./index.html").toString());
  });
 
+app.use('/images', express.static(__dirname + '/images'));
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
